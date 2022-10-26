d3041cf4569b12342721672b9271b4b993fced03
Sayed Ahmed <sayeed205@gmail.com>
2022-10-26 22:04:10 +0530

    add alt ans as comment

--- Easy/14. Longest Common Prefix/14. Longest Common Prefix.py
+++ Easy/14. Longest Common Prefix/14. Longest Common Prefix.py
@@ -13,13 +13,13 @@ class Solution:
 # <====================== alt ans ======================>
 
 
-class Solution:
-    def longestCommonPrefix(self, strs: list[str]) -> str:  # type: ignore
-        if len(strs) == 0:
-            return ""
-        M, m, i = max(strs), min(strs), 0
-        for i in range(min(len(M), len(m))):
-            if M[i] != m[i]:
-                break
-            i += 1
-        return m[:i]
+# class Solution:
+#     def longestCommonPrefix(self, strs: list[str]) -> str:  # type: ignore
+#         if len(strs) == 0:
+#             return ""
+#         M, m, i = max(strs), min(strs), 0
+#         for i in range(min(len(M), len(m))):
+#             if M[i] != m[i]:
+#                 break
+#             i += 1
+#         return m[:i]
